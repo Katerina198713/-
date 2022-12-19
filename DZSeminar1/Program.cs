47def7ed2e99c1ef005d572960eebabb33890350
Ekaterina <katushechka-10@mail.ru>
2022-12-20 01:41:49 +0300

    zadacha4авп

--- DZSeminar1/Program.cs
+++ DZSeminar1/Program.cs
@@ -61,3 +61,21 @@
 // {
 //     Console.WriteLine($"{a} - нечетное число");
 // }
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите любое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int num1 = 1;
+while(num1<N+1)
+if(num1%2==0)
+{
+    Console.Write($"{num1}, ");
+    num1 = num1+1;
+}
+else
+{
+    num1 = num1+1;
+}
