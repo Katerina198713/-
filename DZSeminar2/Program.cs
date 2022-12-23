888aa499c5d65f8959e4456d7c5cdad9655fa06e
Ekaterina <katushechka-10@mail.ru>
2022-12-24 01:37:50 +0300

    Решена задача 21

--- DZSeminar2/Program.cs
+++ DZSeminar2/Program.cs
@@ -43,3 +43,30 @@
 // Console.WriteLine("Введите пятизначное число");
 // int a = Convert.ToInt32(Console.ReadLine());
 // Palindrom(PrintArray(a));
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+void Coordinats( double x, double y,double x1, double y1,double z, double z1)
+{
+   Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((x-x1),2) + Math.Pow((y-y1),2) + Math.Pow((z-z1),2)),2));
+}
+Console.WriteLine("input x=");
+double x=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input y=");
+double y=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input z=");
+double z=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input x1=");
+double x1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input y1=");
+double y1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input z1=");
+double z1=Convert.ToDouble(Console.ReadLine());
+Coordinats(x,y,x1,y1,z,z1);
