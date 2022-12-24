5ab3be547dd89f54d45df5db48c7cfac10328ead
Ekaterina <katushechka-10@mail.ru>
2022-12-25 01:04:39 +0300

    Решена задача 27

--- Семинар 2/Program.cs
+++ Семинар 2/Program.cs
@@ -216,3 +216,22 @@
 
 // string res = Method4(45);
 // Console.WriteLine(res);
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int NatStepenb (int A, int B)
+{
+    int result = Convert.ToInt32(Math.Pow(A, B));
+    return result;
+}
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = NatStepenb(num1, num2);
+Console.WriteLine($"При возведении числа {num1} в {num2}-ю степень получается число {result}");
