70edf8f6e41965a458c7c16cffa12778b210cafe
Ekaterina <katushechka-10@mail.ru>
2022-12-26 23:14:04 +0300

    решена задача 29

--- DZSeminar4/Program.cs
+++ DZSeminar4/Program.cs
@@ -1,4 +1,10 @@
-﻿// int NatStepenb (int A, int B)
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// int NatStepenb (int A, int B)
 // {
 //     int result = Convert.ToInt32(Math.Pow(A, B));
 //     return result;
@@ -19,17 +25,54 @@
 
 // 9012 -> 12
 
-int SumCifr (int num)
-{
-    int summ = 0;
-    for(int i = 0; num >0;i++)
-    {
-        summ = summ + num%10;
-        num = num/10;
-    }
-    return summ;
-}
-Console.WriteLine("Введите целое число:");
-int N = Convert.ToInt32(Console.ReadLine());
-int result = SumCifr(N);
-Console.WriteLine($"Сумма цифр в числе {N} равна {result}");
+// int SumCifr (int num)
+// {
+//     int summ = 0;
+//     for(int i = 0; num >0;i++)
+//     {
+//         summ = summ + num%10;
+//         num = num/10;
+//     }
+//     return summ;
+// }
+// Console.WriteLine("Введите целое число:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int result = SumCifr(N);
+// Console.WriteLine($"Сумма цифр в числе {N} равна {result}");
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+//Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+// int[] CreateRandomArray (int size,int minValue, int maxValue)
+// {
+//     int[] myArray = new int[size];
+//     for(int i=0;i<size;i++)
+//     {
+//         myArray[i] = new Random().Next(minValue,maxValue+1);
+//     }
+//     return myArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     Console.WriteLine("Полученный массив ->:");
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]+" ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+// ShowArray(CreateRandomArray(lenght,min,max));
